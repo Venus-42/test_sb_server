5f07e389c898ed80920778c461484657711fbe30
啟明 <1979533752@qq.com>
2022-11-07 11:15:19 +0800

    feat: add route

--- server.go
+++ server.go
@@ -1,13 +1,28 @@
-package mian
+package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func main() {
 	r := gin.Default()
+	r.StaticFS("/static", http.Dir("static"))
+	//r.StaticFile("/")
 	r.GET("/ping", func(context *gin.Context) {
 		context.JSON(200, gin.H{
 			"message": "pong",
 		})
+	})
+	r.GET("/user/:name", func(context *gin.Context) {
+		name := context.Param("name")
+		context.String(http.StatusOK, "Hello, %s", name)
+	})
+	r.GET("/user/:name/*action", func(context *gin.Context) {
+		name := context.Param("name")
+		action := context.Param("action")
+		message := context.Request.Method + " => " + name + " is " + action
+		context.String(http.StatusOK, message)
 	})
 	err := r.Run()
 	if err != nil {

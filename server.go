733927aaacb9e0051cad3d4d94960e1c13dc3f71
啟明 <1979533752@qq.com>
2022-11-08 15:22:03 +0800

    feat: my mini TCP server

--- server.go
+++ server.go
@@ -3,36 +3,34 @@ package main
 import (
 	"log"
 	"net"
-	"time"
 )
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func handleConn(c net.Conn) {
+	defer c.Close()
+	var buf = make([]byte, 1024)
+	for {
+		log.Println("start to read from conn")
+		n, err := c.Read(buf)
+		checkErr(err)
+		log.Printf("read %d bytes, content is %s\n", n, string(buf[:n]))
+	}
+}
 
 func main() {
 	// listen on 8080 port
 	listen, err := net.Listen("tcp", ":8080")
-	if err != nil {
-		log.Fatal("service failed to start : ", err)
-
-	}
+	checkErr(err)
 	defer listen.Close()
-	log.Println("service started successfully")
-	// block waiting for user to connect
-	accept, err := listen.Accept()
-	if err != nil {
-		log.Fatal(err)
-	}
-	// set timeout
-	err = accept.SetDeadline(time.Now().Add(time.Second))
-	if err != nil {
-		log.Fatal(err)
-	}
-	// set read timeout
-	err = accept.SetReadDeadline(time.Now().Add(time.Second))
-	if err != nil {
-		log.Fatal(err)
-	}
-	// set write timeout
-	err = accept.SetWriteDeadline(time.Now().Add(time.Second))
-	if err != nil {
-		log.Fatal(err)
+	log.Println("service started successfully!")
+	for {
+		conn, err := listen.Accept()
+		checkErr(err)
+		go handleConn(conn)
 	}
 }
